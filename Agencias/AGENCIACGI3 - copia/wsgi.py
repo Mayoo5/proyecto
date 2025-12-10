53e2c53bbafa2b7c1a354b3dcaaad34401437f2e
Mayoo5 <gonzalobergmans@gmail.com>
2025-12-10 09:39:28 -0300

    Preparar para deployment en Render: agregar Procfile y gunicorn

--- Agencias/AGENCIACGI3 - copia/wsgi.py
+++ Agencias/AGENCIACGI3 - copia/wsgi.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Agregar la carpeta del proyecto al path
-project_folder = os.path.expanduser('~/cgiautos-admin')
+project_folder = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, project_folder)
 
 # Importar la aplicación
